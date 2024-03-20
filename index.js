094dac2af74c73802649d69c506e0c3da50de568
Ian <ianmutugi91@gmail.com>
2024-03-20 23:56:08 +0300

    arrow functions assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = function (num1, num2) {
+  if (num2 === 0) {
+    console.log("Cannot divide by zero.");
+    return NaN;
+  }
+  return num1 / num2;
+  const result = divide(2000, 100);
+};
+
+
+const square = (num) => num * num;
+
+const add = (num3, num4) => num3 + num4;
